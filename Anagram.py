d8c4c68898fc9c0564487239022c230194fd6e0a
Faizan <faizanfarhad2211@gmail.com>
2025-09-09 01:12:21 +0530

    question by gfg , level basic

---
+++ Anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def Anagram(self,s1,s2):
+        if len(s1)!= len(s2):
+            return False
+        s1 = ''.join(sorted(s1))
+        s2 = ''.join(sorted(s2))
+        if s1 == s2:
+            return True
+        return False
+        
